ddf01ef05a4748f1ead4eb1adc0c15f9c43e1d2d
Muntaser Syed <jemsbhai@gmail.com>
2023-03-26 13:26:43 -0400

    Create snoredetect.js

---
+++ snoredetect.js
@@ -0,0 +1,66 @@
+
+  // Global variable to store the classifier
+let classifier;
+
+// Label
+let label = 'listening...';
+
+// Teachable Machine model URL:
+let soundModel = 'https://teachablemachine.withgoogle.com/models/WB2lHB7W4/';
+
+
+function preload() {
+  // Load the model
+  classifier = ml5.soundClassifier(soundModel + 'model.json');
+}
+
+function setup() {
+  createCanvas(320, 240);
+  // Start classifying
+  // The sound model will continuously listen to the microphone
+  classifier.classify(gotResult);
+}
+
+function draw() {
+  background(0);
+  // Draw the label in the canvas
+  fill(255);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text(label, width / 2, height / 2);
+}
+
+
+// The model recognizing a sound will trigger this event
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+  
+  if (label == "Snoring") {
+    var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      var raw = JSON.stringify({
+        "action": "addsnore",
+        "dayid": "0"
+      });
+
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+      fetch("https://us-central1-aiot-fit-xlab.cloudfunctions.net/fitbot", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+  }
+  
+}
