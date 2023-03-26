dc00667afc44d9ec5377981f7c2751216586a24a
Muntaser Syed <jemsbhai@gmail.com>
2023-03-26 13:37:04 -0400

    Add files via upload

---
+++ testcocroachdb.py
@@ -0,0 +1,21 @@
+##test cockroachdb
+
+
+
+COCKROACHSTR = "dbname='amber-corgi-9722.hackabull2023' user='hackers' password='DBItvfI18416-dH9gJpJMg' host='amber-corgi-9722.7tt.cockroachlabs.cloud' port='26257'"
+
+
+
+import psycopg2
+
+conn = psycopg2.connect(COCKROACHSTR)
+
+with conn.cursor() as cur:
+    cur.execute("SELECT now()")
+    res = cur.fetchall()
+    conn.commit()
+    print(res)
+    
+    # cur.execute("DROP TABLE READINGS")
+    # conn.commit()
+    
